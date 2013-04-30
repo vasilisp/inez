d115e0e6d54cac3a6c1a672c1158077a47e49db4
Vasilis Papavasileiou <vpap@ccs.neu.edu>
2013-04-30 14:56:26 -0400

    pigeonhole example with UF (currently runtime error)

---
+++ frontend/examples/pigeonhole_uf.ml
@@ -0,0 +1,45 @@
+(* parameters *)
+
+let n_pigeons = 10;;
+let n_holes = n_pigeons - 1;;
+
+assert (n_pigeons >= 0);;
+assert (n_holes >= 0);;
+
+(* utilities *)
+
+let rec make_n ?acc:(acc = []) n ~f =
+  if n <= 0 then
+    List.rev acc
+  else
+    make_n ~acc:(f n :: acc) (n - 1) ~f;;
+
+(* problem entities and instances *)
+
+type pigeon  =  { p_id: int };;
+
+let pigeons  =  make_n n_pigeons ~f:(fun p_id -> {p_id});;
+
+(* UF: pigeons to holes *)
+
+let m =
+  let um = uf (x: int) -> int in
+  fun {p_id} -> um (toi p_id);;
+
+(* constraints *)
+
+(* each pigeon goes to some hole *)
+
+constrain
+  (Formula.forall pigeons
+     ~f:(fun p -> logic (m p >= 1 && m p <= toi n_holes)));;
+
+(* no pair of pigeons co-inhabit a hole *)
+
+constrain
+  (Formula.forall_pairs pigeons
+     ~f:(fun p1 p2 -> logic (not (m p1 == m p2))));;
+
+(* call solver *)
+
+solve ();;
